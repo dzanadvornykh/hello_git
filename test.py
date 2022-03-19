3489bf111a2c76817e1013ec1a4c47456ed02db1
dzanadvornykh <dmitriy.zanadvornykh@quantori.com>
2022-03-19 20:30:26 +0300

    one

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import time
+from typing import Callable
 
 
 def hello():
     print('world')
+
+
+def prikol(func: Callable) -> None:
+    print('hi')
+    def inner(*args):
+        func(*args)
+    print('by')
+
 
 print(time.time())
 print()
