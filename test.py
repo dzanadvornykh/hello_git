8c4e6361c0f40761585679cf4e76cc63bf87d765
dzanadvornykh <dmitriy.zanadvornykh@quantori.com>
2022-03-19 21:17:36 +0300

    two

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ def hello():
     print('world')
 
 
-def prikol(func: Callable) -> None:
+def prikol(func: Callable) -> Callable:
     print('hi')
     def inner(*args):
         func(*args)
     print('by')
-
+    return inner
 
 print(time.time())
 print()
